20f94f26572332a3624f69a4e698be426ea92573
Seemant Kulleen <seemantk@gmail.com>
2014-02-12 19:27:00 -0800

    Clean up strings into arrays of strings joined at output time.  This lets us stay within 80 chars per line.  Especially with 2-space tabstops.

--- lib/cinch/plugins/rupert.rb
+++ lib/cinch/plugins/rupert.rb
@@ -11,51 +11,75 @@ module Cinch::Plugins
 			@mic = Buff::Client.new "YOURKEYHERE"
 		end
 
-		match(/profiles/, method: :profiles)
+		listen_to :join
+		def listen(m)
+			profiles(m) if m.user.nick == bot.nick
+		end
+
+
+		match /profiles/, method: :profiles
 		def profiles(m)
+			replies = []
 			@mic.profiles.each do |profile|
-				reply = "#{profile.formatted_service} (#{profile.formatted_username}). "
+				reply = []
+				reply << "#{profile.service.upcase}/#{profile.formatted_username}"
 				unless profile.statistics.empty?
 					stats = profile.statistics.map { |k,v|
 							{k => v} if v > 0
-						}.compact.reduce(:merge)
+						}.compact.reduce :merge
 					
+					# Each network has different nouns
+					# ("followers", "likes", "impressions", etc.).
+					# Why does Google+ attach "_28" to the field names?
 					unless stats.nil?
-						reply += "Social: " + stats.map { |k, v|
-							"#{v} #{k.capitalize.gsub('_28', '').gsub('_', ' ')}"
-						}.join(", ") + " "
+						reply << "Social: " + stats.map { |k, v|
+							"#{v} #{k.gsub(/_\d+/, ' ').strip.capitalize}"
+						}.join(", ")
 					end
 				end
 
-				reply += "Messages: #{profile.counts.sent} sent, #{profile.counts.pending} pending, #{profile.counts.drafts} draft."
+				# Create a messages summary of sent, drafts, pending messages
+				# Only bother mentioning the ones that are nonzero
+				reply << "Messages: " + profile.counts.map { |k,v|
+						"#{v} #{k}" if v != 0
+					}.compact.join(", ")
 
-				m.reply(reply)
+				replies << reply
 			end
+				m.channel.topic	= "#{replies.map { |r| "#{r.join('. ')}" }.join(" || ")}"
 		end
 
-		match(/publish (.*)/, method: :publish)
+		match /publish (.*)/, method: :publish
 		def publish(m, content)
-			profile_ids = @mic.profiles.map { |p| p.id }
 
 			response = @mic.create_update({
 					:body => {
 						:text => content,
-						:profile_ids => profile_ids
+						:profile_ids => @mic.profiles.map { |p| p.id }
 					}
 				})
 			
-			success = "Success"
-			success = "Failure" unless response.success
-			m.reply("#{success.capitalize}!")
+			m.reply response.success ? "Success!" : "Failure!"
 
-			# Find what is not in the "buffer" state:
-			response.updates.each do |message|
+			response.updates.map.each do |message|
 				due = DateTime.strptime("#{message.due_at}", '%s')
-				m.reply("#{message.profile_service.capitalize}: ID: #{message.id}, Status: #{message.status}, Due: #{due.to_s}")
+
+				m.reply "#{message.profile_service.capitalize}: " +
+					[
+						["ID: #{message.id}"],
+						["Status: #{message.status}"],
+						["Due: #{due.to_s}"]
+					].join(", ")
 			end
 
-			cap = response.buffer_count * 100 / response.buffer_percentage
-			m.reply("#{response.buffer_count} messages in queue.  Using #{response.buffer_percentage}% of buffer.  #{cap - response.buffer_count} messages remain.")
+
+			cap = 100 * response.buffer_count / response.buffer_percentage
+
+			m.reply [
+					"#{response.buffer_count}	messages in queue",
+					"Using #{response.buffer_percentage}% of buffer",
+					"#{cap - response.buffer_count} messages remain."
+				].join(". ")
 
 			profiles(m)
 		end
